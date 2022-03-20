4bcda027e124c703c35775712727c6a7c1932a21
coderGirlSu <suzhang79@gmail.com>
2022-03-20 12:08:07 +1100

    added email-icon and h1 font styles

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,7 @@
-function burgerMenu(){
-    let menu = document.getElementById("burger-menu");
+// A function to show and hide the hamburgermenu
+function burgerMenu() {
+    // Find the element by ID called burger-menu in html
+    let menu = document.getElementById("burger-menu"); 
+    // Add or remove the "visible" class
     menu.classList.toggle("visible");
 }
